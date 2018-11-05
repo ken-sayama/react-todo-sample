a01364a80c7358be6ee644d4c5105fe86421a95b
ken-sayama <sayamaken0402@gmail.com>
2018-11-05 15:18:32 +0900

    fix App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="siimple-box siimple-box--orange">
+        <h1 className="siimple-box-title">React Todo App</h1>
       </div>
     );
   }
